bfe1026d148c0b00013cceb019bc6c2ef59fe8bb
lfahm <142011712+lfahm@users.noreply.github.com>
2023-08-13 12:45:33 +0200

    Add files via upload

---
+++ ah.py
@@ -0,0 +1,60 @@
+import calendar
+year=2023
+month=1
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=2
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=3
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=4
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=5
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=6
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=7
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=8
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=9
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=10
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=11
+x=calendar.month(year,month)
+print(x)
+import calendar
+year=2023
+month=12
+x=calendar.month(year,month)
+print(x)
